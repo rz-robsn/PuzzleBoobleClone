ca08d8c5c3e50ef7d27168fd100979d6afcb8723
Raza <rz.robsn@gmail.com>
2012-10-15 20:22:20 -0300

    added AimingArrow.cs rotation controls.

--- PuzzleBoobleClone/PuzzleBoobleClone/GameElements/AimingArrow.cs
+++ PuzzleBoobleClone/PuzzleBoobleClone/GameElements/AimingArrow.cs
@@ -4,23 +4,58 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace PuzzleBoobleClone.GameElements
 {
     public class AimingArrow : GameElement
     {
         private static Rectangle SPRITE_RECTANGLE = new Rectangle(0, 514, 23, 56);
-        private Vector2 Position = new Vector2(295,322);
+        private static Vector2 ROTATION_CENTER = new Vector2(11, 32.5f);
+
+        private Vector2 Position = new Vector2(317, 384);
+        private float angle = (float)Math.PI/2;
+        private float RotationSpeed = (float)Math.PI/90;
 
         public AimingArrow() { }
 
-        public void Update(GameTime gameTime, Game1 game){}
+        public void Update(GameTime gameTime, Game1 game)
+        {
+            KeyboardState keyBoardState = Keyboard.GetState();
+
+            if (keyBoardState.IsKeyDown(Keys.Right))
+            {
+                angle = MathHelper.Clamp(angle + RotationSpeed, 0, (float)Math.PI);
+            }
+
+            if (keyBoardState.IsKeyDown(Keys.Left))
+            {
+                angle = MathHelper.Clamp(angle - RotationSpeed, 0, (float)Math.PI);
+            }
+        }
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch, Game1 game)
         {
             Texture2D spriteSheet = game.GameElements.SpriteSheet;
 
-            spriteBatch.Draw(spriteSheet, Position, SPRITE_RECTANGLE, Color.White, 0f, new Vector2(0, 0), 2.0f, SpriteEffects.None, 0.8f);
+            spriteBatch.Draw(texture: spriteSheet,
+                        position: Position,
+                        sourceRectangle: SPRITE_RECTANGLE,
+                        color: Color.White,
+                        rotation: angle - (float)Math.PI/2, 
+                        origin: ROTATION_CENTER,
+                        scale: 2.0f,
+                        effects: SpriteEffects.None,
+                        layerDepth: 0.8f);
+        }
+
+        /// <summary>
+        /// Returns a unit Vector that has the same direction as the
+        /// Arrow
+        /// </summary>
+        public Vector2 GetDirectionVector()
+        {
+            return new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
         }
     }
 }
