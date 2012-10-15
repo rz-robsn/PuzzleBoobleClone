1a2f67317cfae71d0f5bcab672b893e70823e878
Raza <rz.robsn@gmail.com>
2012-10-15 02:45:21 -0300

    added 1st part of the machine background Element.

--- PuzzleBoobleClone/PuzzleBoobleClone/GameElements/BagAndLauncherMachine.cs
+++ PuzzleBoobleClone/PuzzleBoobleClone/GameElements/BagAndLauncherMachine.cs
@@ -13,8 +13,10 @@ namespace PuzzleBoobleClone.GameElements
         /// Rectangle of the bag Image on the SpriteSheet
         /// </summary>
         private static Rectangle BAG_RECTANGLE = new Rectangle(344, 834, 57, 25);
-
         private static Vector2 BAG_POSITION = new Vector2(168, 385);
+
+        private static Rectangle MACHINE_RECTANGLE = new Rectangle(204, 813, 60, 25);
+        private static Vector2 MACHINE_POSITION = new Vector2(253, 385);
 
         public BagAndLauncherMachine() { }
 
@@ -23,8 +25,9 @@ namespace PuzzleBoobleClone.GameElements
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch, Game1 game) 
         {
             Texture2D spriteSheet = game.GameElements.SpriteSheet;
-            //spriteBatch.Draw(spriteSheet, new Vector2(0, 0), BAG_RECTANGLE, Color.White);
             spriteBatch.Draw(spriteSheet, BAG_POSITION, BAG_RECTANGLE, Color.White, 0f, new Vector2(0, 0), 2.0f, SpriteEffects.None, 0);
+            spriteBatch.Draw(spriteSheet, MACHINE_POSITION, MACHINE_RECTANGLE, Color.White, 0f, new Vector2(0, 0), 2.0f, SpriteEffects.None, 0);
+
         }
     }
 }
